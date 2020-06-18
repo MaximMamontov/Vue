066400842ad598e051a393be97c1fee6b8a1fd0b
Maks <mms98@inbox.ru>
2020-06-18 16:10:19 +0300

    Add login page and validation

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-navigation-drawer
+            app
+            v-model="drawer"
+            temporary
+    >
+      <v-list>
+        <v-list-item
+                v-for="link of links"
+                :key="link.title"
+                :to="link.url"
+                router
+                exact
+        >
+          <v-list-item-icon>
+            <v-icon >{{link.icon}}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title v-text="link.title" ></v-list-item-title>
+          </v-list-item-content>
+
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-card
+            color="grey lighten-4"
+            flat
+            height="70px"
+            tile
+    >
+      <v-toolbar dense dark color="primary" >
+        <v-app-bar-nav-icon
+        @click=" drawer = !drawer"
+        class="hidden-md-and-up"
+        ></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Ad Application</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn
+                  v-for="link in links"
+                  :key="link.title"
+                  :to="link.url"
+                  flat
+          >
+            <v-icon left>{{link.icon}}</v-icon>
+            {{link.title}}
+          </v-btn>
+        </v-toolbar-items>
+
+      </v-toolbar>
+    </v-card>
+    <v-content>
+
+        <router-view></router-view>
+
+    </v-content>
+
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      drawer: false,
+      links: [
+        {title: 'Login', icon: 'mdi-lock', url: '/login'},
+        {title: 'Registration', icon: 'mdi-face', url: '/registration'},
+        {title: 'Orders', icon: 'mdi-bookmark', url: '/orders'},
+        {title: 'New ad', icon: 'mdi-note', url: '/new'},
+        {title: 'My ads', icon: 'mdi-linux', url: '/list'}
+      ]
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
