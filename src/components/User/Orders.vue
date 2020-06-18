09b50c3f3a3196f29a384d42a8f1d0a502bb23ee
Maks <mms98@inbox.ru>
2020-06-18 18:54:56 +0300

    Orders list page complited

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,8 +1,33 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>Orders</h1>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb-3">Orders</h1>
+        <v-list two-line subheader>
+          <v-list-item
+                  avatar
+                  v-for="order in orders"
+                  :key="order.id"
+          >
+            <v-list-item-action>
+              <v-checkbox
+                      color="success"
+                      :input-value="order.done"
+                      @change="markDone(order)"
+              ></v-checkbox>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>{{order.name}}</v-list-item-title>
+              <v-list-item-subtitle>{{order.phone}}</v-list-item-subtitle>
+            </v-list-item-content>
+            <v-list-item-action>
+              <v-btn
+                      :to="'/ad/' + order.adId"
+                      class="primary"
+              >Open</v-btn>
+            </v-list-item-action>
+          </v-list-item>
+        </v-list>
       </v-flex>
     </v-layout>
   </v-container>
@@ -10,8 +35,23 @@
 
 <script>
   export default {
-    data(){
-      return{}
+    data () {
+      return {
+        orders: [
+          {
+            id: 'fds3',
+            name: 'Maxim_Mamontov',
+            phone: '8029-777-66-55',
+            adId: '123',
+            done: false
+          }
+        ]
+      }
+    },
+    methods: {
+      markDone (order) {
+        order.done = true
+      }
     }
   }
 </script>
